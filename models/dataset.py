9ecc07fe87e5e833d3885b4166a0e73697440276
theosth <67734722+theosth@users.noreply.github.com>
2025-03-17 17:52:16 +0100

    changes to dataset and reconstruction test

--- models/dataset.py
+++ models/dataset.py
@@ -1,5 +1,7 @@
+import random
 import torch
 import os
+import csv
 from torch.utils.data import Dataset, DataLoader
 import pandas as pd
 from PIL import Image
@@ -9,12 +11,218 @@ import numpy as np
 import sys
 from pathlib import Path
 from torchvision.datasets import ImageFolder
-from torchvision import transforms
+from torchvision import transforms, datasets
 
 
 sys.path.append(str(Path(__file__).parent.parent))
 
+
+class SpectrogramDataset(Dataset):
+
+    def __init__(self, config):
+        super(SpectrogramDataset, self).__init__()
+        self.image_dir_path = config["processed_spectograms_dataset_folderpath"]
+        self.data = datasets.ImageFolder(root=self.image_dir_path, transform=self._get_transform())
+
+    def __getitem__(self, idx):
+        x, y = self.data[idx]
+        return x, y
+
+    def __len__(self):
+        return len(self.data)
+
+    def _get_transform(self):
+        """
+        Define the transformations to be applied to the images.
+        :return: Transformations
+        """
+        return transforms.Compose(
+            [
+                # add crop from 130 to 128
+                # ! If the chunk size is different, this needs to be changed
+                transforms.Lambda(lambda x: x.crop((0, 0, 128, 128))),  # Crop to 128x128
+                transforms.Grayscale(),  # Needed because ImageFolder by default converts to RGB -> convert back
+                transforms.ToTensor(),  # Automatically normalizes [0,255] to [0,1]
+            ]
+        )
+
+
+# class MultiFolderSpectrogramPairDataset(Dataset):
+#     def __init__(self, root_folder, num_pairs, transform=None):
+#         """
+#         Args:
+#             root_folder (str): Path to the root directory containing subfolders for each label.
+#             num_pairs (int): Number of (sample, sample) pairs to generate.
+#             transform: Transformation to apply to the images.
+#         """
+#         self.num_pairs = num_pairs
+#         self.transform = transform if transform is not None else self._get_transform()
+
+#         # Get the list of subfolder paths
+#         self.subfolders = [
+#             os.path.join(root_folder, folder)
+#             for folder in os.listdir(root_folder)
+#             if os.path.isdir(os.path.join(root_folder, folder))
+#         ]
+
+#         # Create a separate ImageFolder dataset for each subfolder.
+#         # We'll use the subfolder name as the label.
+#         self.datasets = {}
+#         for folder in self.subfolders:
+#             label = os.path.basename(folder)
+#             self.datasets[label] = datasets.ImageFolder(root=folder, transform=self.transform)
+
+#         self.labels = list(self.datasets.keys())
+
+#         if len(self.labels) < 2:
+#             raise ValueError("Need at least two classes to form pairs with different labels.")
+
+#     def __len__(self):
+#         return self.num_pairs
+
+#     def __getitem__(self, idx):
+#         # Randomly select two distinct labels
+#         label1, label2 = random.sample(self.labels, 2)
+#         dataset1 = self.datasets[label1]
+#         dataset2 = self.datasets[label2]
+
+#         # Randomly sample one image from each dataset.
+#         img1, _ = dataset1[random.randint(0, len(dataset1) - 1)]
+#         img2, _ = dataset2[random.randint(0, len(dataset2) - 1)]
+
+#         return (img1, label1), (img2, label2)
+
+#     def _get_transform(self):
+#         """
+#         Define the transformations to be applied to the images.
+#         :return: Transformations
+#         """
+#         return transforms.Compose(
+#             [
+#                 # add crop from 130 to 128
+#                 # ! If the chunk size is different, this needs to be changed
+#                 transforms.Lambda(lambda x: x.crop((0, 0, 128, 128))),  # Crop to 128x128
+#                 transforms.Grayscale(),  # Needed because ImageFolder by default converts to RGB -> convert back
+#                 transforms.ToTensor(),  # Automatically normalizes [0,255] to [0,1]
+#             ]
+#         )
+
+
+class SpectrogramPairDataset(Dataset):
+    def __init__(self, root_folder, pairing_file, transform=None):
+        """
+        Args:
+            root_folder (str): Root directory with subfolders (each for one label).
+            pairing_file (str): Path to the CSV file with predetermined pairings.
+            transform: Transformations to apply to each image.
+        """
+        self.root_folder = root_folder
+        self.pairing_file = pairing_file
+        self.transform = transform if transform is not None else self._get_transform()
+        
+        # Load the precomputed pairs from the CSV file.
+        # Each row in the CSV should contain: label1, idx1, label2, idx2
+        self.pairs = []
+        with open(self.pairing_file, 'r') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                # Convert index strings to integers.
+                self.pairs.append((row[0], int(row[1]), row[2], int(row[3])))
+        
+        # Build a dictionary of ImageFolder datasets keyed by label.
+        self.datasets = {}
+        # Sorting the subfolders ensures deterministic order.
+        for folder in sorted(os.listdir(root_folder)):
+            folder_path = os.path.join(root_folder, folder)
+            if os.path.isdir(folder_path):
+                # Assume the folder name is the label.
+                self.datasets[folder] = datasets.ImageFolder(root=folder_path, transform=self.transform)
+        
+    def __len__(self):
+        return len(self.pairs)
+    
+    def __getitem__(self, index):
+        # Use the index to get the predetermined pairing.
+        label1, idx1, label2, idx2 = self.pairs[index]
+        img1, _ = self.datasets[label1][idx1]
+        img2, _ = self.datasets[label2][idx2]
+        return (img1, label1), (img2, label2)
+    
+    @classmethod
+    def _get_transform(cls):
+        """
+        Define the transformations to be applied to the images.
+        :return: Transformations
+        """
+        return transforms.Compose(
+            [
+                # add crop from 130 to 128
+                # ! If the chunk size is different, this needs to be changed
+                transforms.Lambda(lambda x: x.crop((0, 0, 128, 128))),  # Crop to 128x128
+                transforms.Grayscale(),  # Needed because ImageFolder by default converts to RGB -> convert back
+                transforms.ToTensor(),  # Automatically normalizes [0,255] to [0,1]
+            ]
+        )
+
+
+    @classmethod
+    def generate_pairings(cls, root_folder, output_file_path='spectrogram_pair_dataset_pairings.csv', num_pairs=15000):
+        """
+        Generates a CSV file containing the predetermined pairings.
+        
+        Args:
+            root_folder (str): Root directory with subfolders for each label.
+            output_file (str): Path where the CSV file will be saved.
+            num_pairs (int): Number of pairs to generate.
+        """
+        # List of labels (i.e. subfolder names) sorted deterministically.
+        labels = sorted([
+            folder for folder in os.listdir(root_folder)
+            if os.path.isdir(os.path.join(root_folder, folder))
+        ])
+        
+        if len(labels) < 2:
+            raise ValueError("Need at least two classes to form pairs.")
+        
+        # Create ImageFolder datasets for each label.
+        datasets_dict = {}
+        for label in labels:
+            folder_path = os.path.join(root_folder, label)
+            datasets_dict[label] = datasets.ImageFolder(root=folder_path, transform=cls._get_transform())
+        
+        pairs = []
+        # We precompute the pairs and save them as a file. Like this the future sampling is deterministic.
+        rng = np.random.RandomState(42)
+        for _ in range(num_pairs):
+            # Randomly select two distinct labels.
+            label1, label2 = rng.choice(labels, size=2, replace=False)
+            ds1, ds2 = datasets_dict[label1], datasets_dict[label2]
+            # Randomly select indices within each dataset.
+            idx1 = rng.randint(0, len(ds1))
+            idx2 = rng.randint(0, len(ds2))
+            pairs.append((label1, idx1, label2, idx2))
+        
+        # Write the pairs to a CSV file.
+        with open(output_file_path, 'w', newline='') as f:
+            writer = csv.writer(f)
+            for pair in pairs:
+                writer.writerow(pair)
+        print(f"Pairings saved to {output_file_path}")
+
 def prepare_dataset(config):
+    dataset = SpectrogramDataset(config)
+
+    train_size = int(0.8 * len(dataset))
+    test_size = len(dataset) - train_size
+    train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
+
+    train_loader = DataLoader(train_dataset, batch_size=config["batch_size"], shuffle=True, num_workers=0)
+    test_loader = DataLoader(test_dataset, batch_size=config["batch_size"], shuffle=False, num_workers=0)
+
+    return train_loader, test_loader
+
+
+def prepare_dataset_other(config):
 
     transform = transforms.Compose(
         [
@@ -34,6 +242,31 @@ def prepare_dataset(config):
 
     # TODO: Remove num_workers=0, so far it doesnt work with multiprocessing
     train_loader = DataLoader(train_dataset, batch_size=config["batch_size"], shuffle=True, num_workers=0)
-    test_loader = DataLoader(test_dataset, batch_size=config["batch_size"], shuffle=False, num_workers=4)
+    test_loader = DataLoader(test_dataset, batch_size=config["batch_size"], shuffle=False, num_workers=0)
 
     return train_loader, test_loader
+
+
+if __name__ == "__main__":
+
+    print("Testing SpectrogramDataset\n")
+    dataset = SpectrogramDataset(config)
+    print(len(dataset))
+    train_loader, test_loader = prepare_dataset(config)
+    print(len(train_loader))
+    print(len(test_loader))
+
+    a = 0
+
+    for batch in train_loader:
+        a += 1
+        if a > 10:
+            break
+        print(batch[0].shape)
+        print(batch[1])
+
+        print("--------------------------------")
+
+    print("Testing SpectrogramPairDataset\n")
+    dataset = SpectrogramPairDataset(config["processed_spectograms_dataset_folderpath"], config["pairing_file_path"])
+    
--- models/tests.py
+++ models/tests.py
@@ -4,7 +4,7 @@ from model import ddim_sample, ForwardDiffusion, UNet
 from model import SpectrogramEncoder, SpectrogramDecoder
 from config import config
 from model import StyleEncoder
-from dataset import prepare_dataset
+from dataset import SpectrogramDataset, prepare_dataset
 import torch
 import matplotlib.pyplot as plt
 
@@ -326,41 +326,32 @@ def test_autoencoder_reconstruction():
     encoder.eval()
     decoder.eval()
 
-    # get loaders
+    # Load dataset
     train_loader, test_loader = prepare_dataset(config)
+    # Get one batch from the test_loader (batch size should be > 5)
+    images, labels = next(iter(test_loader))
+    num_examples = min(5, images.size(0))
+    fig, axes = plt.subplots(2, num_examples, figsize=(15, 6))
     
-    # Select 5 random indices
-    indices = torch.randint(0, len(test_loader), (5,))
-
-    # Get some test samples
-    samples = []
     with torch.no_grad():
-        for i, (image, label) in enumerate(test_loader):
-            if i >= 4:  # Get 4 samples
-                break
-            image = image.to(device)
+        for idx in range(num_examples):
+            # Get image and label from the batch
+            image = images[idx].unsqueeze(0).to(device)  # Add batch dimension and send to device
+            label = labels[idx]
             
-            # Get reconstruction
+            # Get reconstruction from the encoder and decoder
             latent = encoder(image)
             reconstruction = decoder(latent)
             
-            # Store original and reconstruction
-            samples.append((image.cpu(), reconstruction.cpu(), label))
-    
-    
-    # Create figure
-    fig, axes = plt.subplots(2, 5, figsize=(15, 6))
-    # Plot results
-    for idx, (original, reconstruction, label) in enumerate(samples):
-        # Plot original
-        axes[0, idx].imshow(original.squeeze(), cmap='gray')
-        axes[0, idx].set_title(f'Original ({label[0]})')
-        axes[0, idx].axis('off')
-        
-        # Plot reconstruction
-        axes[1, idx].imshow(reconstruction.squeeze(), cmap='gray')
-        axes[1, idx].set_title('Reconstruction')
-        axes[1, idx].axis('off')
+            # Plot original image
+            axes[0, idx].imshow(image.squeeze().cpu(), cmap='gray')
+            axes[0, idx].set_title(f'Original\n{label}')
+            axes[0, idx].axis('off')
+            
+            # Plot reconstructed image
+            axes[1, idx].imshow(reconstruction.squeeze().cpu(), cmap='gray')
+            axes[1, idx].set_title('Reconstruction')
+            axes[1, idx].axis('off')
     
     plt.tight_layout()
     plt.savefig('models/plots/autoencoder_reconstructions.png')
