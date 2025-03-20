fddc54d8e65df26f8c80c46bb78f5c33e75f04e0
prio <prioteasa.andrei77@gmail.com>
2025-03-20 18:52:06 +0100

    ready for big training the ldm; the auto enc is pretrained

--- models/config.py
+++ models/config.py
@@ -4,7 +4,7 @@ config = {
     'learning_rate_factor': 0.5,
     'learning_rate_patience': 5,
     'learning_rate_min': 1e-6,
-    'num_epochs': 10,
+    'num_epochs': 100,
     'batch_size': 64,
     'style_loss_weight': 0.1,
     'latent_dim_encoder': 32,
